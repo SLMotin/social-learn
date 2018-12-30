667aa81abc53c42100314785e68a1cb23c42b3eb
danilocaverna <nq3i4fsx@hotmail.com>
2018-12-30 20:15:56 -0300

    service login + login page (subistituir antigo home)

---
+++ src/service/authentication.servise.ts
@@ -0,0 +1,20 @@
+import { Storage } from '@ionic/storage';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+    private places: {title: string}[] = [];
+
+    constructor(private storage: Storage){}
+
+    addPlace(place: {title: string}){
+        this.places.push(place);
+        this.storage.set('places', this.places);
+    }
+    async getPlaces(){
+        this.places = await this.storage.get('places');
+        if(this.places === null)
+            this.places = [];
+        return this.places.slice();
+    }
+}
