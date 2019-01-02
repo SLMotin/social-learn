b8a8a2e880c2a59b311278241270913165d9783e
danilocaverna <nq3i4fsx@hotmail.com>
2019-01-02 12:31:19 -0300

    loading teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule, Loading } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule, LoadingController } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule } from '@ionic/storage';
@@ -11,14 +11,12 @@ import { AuthService } from '../services/authentication.service';
 
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
-import { LoadingPage } from '../pages/loading/loading';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     LoginPage,
-    LoadingPage
   ],
   imports: [
     BrowserModule,
@@ -30,14 +28,14 @@ import { LoadingPage } from '../pages/loading/loading';
     MyApp,
     HomePage,
     LoginPage,
-    LoadingPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     Storage,
-    AuthService
+    AuthService,
+    LoadingController
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 import { LoginPage } from '../login/login';
 import { AuthService } from '../../services/authentication.service';
-import { LoadingPage } from '../loading/loading';
 
 @Component({
   selector: 'page-home',
@@ -10,15 +10,16 @@ import { LoadingPage } from '../loading/loading';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private authService: AuthService) {
-    
-  }
-  async ionViewWillEnter(){
-    this.navCtrl.push(LoadingPage);
+  constructor(public navCtrl: NavController, private authService: AuthService, public loadingCtrl: LoadingController) {}
 
+  async ionViewWillEnter(){
+    const loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    loader.present();
     var isAuth = await this.authService.isAuthenticated();
-    this.navCtrl.pop();
     if(!isAuth)
       this.navCtrl.push(LoginPage);
+    loader.dismiss();
   }
 }
